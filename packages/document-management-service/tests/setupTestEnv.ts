d74eb09ca452dc8ccef7db86ef045e1edc6d368e
Martyn Janes <martyn@obany.com>
2025-02-24 16:02:33 +0200

    Update test env

--- packages/document-management-service/tests/setupTestEnv.ts
+++ packages/document-management-service/tests/setupTestEnv.ts
@@ -1,7 +1,6 @@
 // Copyright 2024 IOTA Stiftung.
 // SPDX-License-Identifier: Apache-2.0.
-import path from "node:path";
-import { Is, RandomHelper } from "@twin.org/core";
+import { RandomHelper } from "@twin.org/core";
 import { Bip39 } from "@twin.org/crypto";
 import { MemoryEntityStorageConnector } from "@twin.org/entity-storage-connector-memory";
 import { EntityStorageConnectorFactory } from "@twin.org/entity-storage-models";
@@ -25,21 +24,6 @@ import {
 	type WalletAddress
 } from "@twin.org/wallet-connector-entity-storage";
 import { WalletConnectorFactory } from "@twin.org/wallet-models";
-import * as dotenv from "dotenv";
-
-console.debug("Setting up test environment from .env and .env.dev files");
-
-dotenv.config({ path: [path.join(__dirname, ".env"), path.join(__dirname, ".env.dev")] });
-
-if (!Is.stringValue(process.env.TEST_MNEMONIC)) {
-	// eslint-disable-next-line no-restricted-syntax
-	throw new Error(
-		`Please define TEST_MNEMONIC as a 24 word mnemonic either as an environment variable or inside an .env.dev file
-		 e.g. TEST_MNEMONIC="word0 word1 ... word23"
-		 You can generate one using the following command
-		 npx "@twin.org/crypto-cli" mnemonic --env ./tests/.env.dev --env-prefix TEST_`
-	);
-}
 
 initSchemaVault();
 initSchemaIdentity();
@@ -88,7 +72,12 @@ export async function setupTestEnv(): Promise<void> {
 		.fn()
 		.mockImplementationOnce(length => new Uint8Array(length).fill(99))
 		.mockImplementation(length => new Uint8Array(length).fill(88));
-	Bip39.randomMnemonic = vi.fn().mockImplementation(() => process.env.TEST_MNEMONIC);
+	Bip39.randomMnemonic = vi
+		.fn()
+		.mockImplementation(
+			() =>
+				"life first castle choose joke eyebrow middle speak lucky improve awesome common energy oval use scare water cluster update steak endorse sweet festival error"
+		);
 
 	const testIdentityConnector = IdentityConnectorFactory.get("identity");
 	const testVaultConnector = VaultConnectorFactory.get("vault");
